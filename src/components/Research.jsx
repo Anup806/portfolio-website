97660b958fee8f25fe51cfe9f1e2a2287066be15
Anup806 <raianup806@gmail.com>
2026-08-09 15:05:53 +0545

    Modified research points

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -17,7 +17,7 @@ function Research() {
 
           <ul className="research-points">
             <li>Designed an adaptive Augmented Reality system using Deep Knowledge Tracing (DKT)</li>
-            <li>Developed system architecture following Research-Through-Design (RTD) methodology</li>
+            <li>Developed system architecture following interative Research-Through-Design (RTD) methodology and Pilot Testing</li>
             <li>Achieved Accuracy: 0.84 and PR-AUC: 0.82 in real-world classroom deployment</li>
           </ul>
 
